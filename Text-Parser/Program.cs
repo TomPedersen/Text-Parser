f1d1486121a4f3da09c558d9e8476593f24664ef
TomPedersen <Knives89@gmail.com>
2015-07-10 17:40:56 +1200

    code planning, research and pseudocoding

--- Text-Parser/Program.cs
+++ Text-Parser/Program.cs
@@ -10,6 +10,66 @@ namespace Text_Parser
     {
         static void Main(string[] args)
         {
+            string[] array =
+	        {
+	            "Do you like this site?",
+	            "--cool--",
+	            "...ok!",
+	            "None",
+	            ""
+	        };
+            // Call method on each string.
+            foreach (string value in array)
+            {
+                Console.WriteLine(Program.TrimPunctuation(value));
+                Console.ReadLine();
+            }
+        }
+
+        static string TrimPunctuation(string value)
+        {
+            // Count start punctuation.
+            int removeFromStart = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsPunctuation(value[i]))
+                {
+                    removeFromStart++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // Count end punctuation.
+            int removeFromEnd = 0;
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                if (char.IsPunctuation(value[i]))
+                {
+                    removeFromEnd++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            // No characters were punctuation.
+            if (removeFromStart == 0 &&
+                removeFromEnd == 0)
+            {
+                return value;
+            }
+            // All characters were punctuation.
+            if (removeFromStart == value.Length &&
+                removeFromEnd == value.Length)
+            {
+                return "";
+            }
+            // Substring.
+            return value.Substring(removeFromStart,
+                value.Length - removeFromEnd - removeFromStart);
         }
     }
 }
