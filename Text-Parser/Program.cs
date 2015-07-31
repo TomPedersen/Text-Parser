d651d6663daaebeddb3670ddc8e721e215665317
TomPedersen <Knives89@gmail.com>
2015-07-31 16:38:50 +1200

    remove display method and turned other methods public. Also utilised string format instead of concatenation

--- Text-Parser/Program.cs
+++ Text-Parser/Program.cs
@@ -16,11 +16,20 @@ namespace Text_Parser
 
         static void Main(string[] args)
         {
-            TextAnalyzer TomsReader = new TextAnalyzer();
+            TextAnalyzer tomsReader = new TextAnalyzer();
 
-            var sampleText = "Hold your ground, hold your ground! Sons of Gondor, of Rohan, my brothers! I see in your eyes the same fear that would take the heart of me. A day may come when the courage of men fails, when we forsake our friends and break all bonds of fellowship, but it is not this day. An hour of wolves and shattered shields, when the age of men comes crashing down! But it is not this day! This day we fight! By all that you hold dear on this good Earth, I bid you stand, Men of the West!";
+            var Text = "Call me Ishmael. Some years ago — never mind how long precisely — having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen, and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off — then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.";
 
-            TomsReader.FullAnalysis(sampleText);
+
+            var thirdLargestWord = string.Join(", ", tomsReader.ThirdLongestWord(tomsReader.ToWordArray(Text)).ToList());
+            var thirdLargestWordCharCount = tomsReader.ThirdLongestWord(tomsReader.ToWordArray(Text)).Key;
+
+            Console.WriteLine("There are {0} sentences.", tomsReader.ToSentenceArray(Text).Length);
+            Console.WriteLine("There are {0} total words.", tomsReader.ToWordArray(Text).Length);
+            Console.WriteLine("The longest sentence is: {0}", tomsReader.LongestSentence(tomsReader.ToSentenceArray(Text)));
+            Console.WriteLine("The most frequently used word is: \"{0}\"", tomsReader.MostFrequentWord(tomsReader.ToWordArray(Text)));
+            Console.WriteLine("The third longest word(s): {0}  Number of characters: {1}", thirdLargestWord, thirdLargestWordCharCount);
+            Console.ReadLine();
         }
     }
 }
--- Text-Parser/TextAnalyzer.cs
+++ Text-Parser/TextAnalyzer.cs
@@ -9,50 +9,36 @@ namespace Text_Parser
 {
     class TextAnalyzer
     {
-        private string RemoveAllPunctuation ( string text )
+        public string RemoveAllPunctuation ( string text )
         {
             return Regex.Replace(text, @"[^\w\s]", ""); //Removes all punctuation, only keeping words/numbers/whitespace
         }
 
-        private string[] ToSentenceArray (string text) 
+        public string[] ToSentenceArray (string text) 
         {
             return Regex.Split(text, @"(?<=[\.!\?])\s+"); //Divides text into array by splitting at !, ? and . while keeping these punctuators
         }
 
-        private string LongestSentence (string[] text)
+        public string LongestSentence (string[] text)
         {
             return text.OrderByDescending(s => s.Length).First(); //Orders split sentence array by descending length and returns first(largest)
         }
 
-        private string[] ToWordArray (string text)
+        public string[] ToWordArray (string text)
         {
             return RemoveAllPunctuation(text).Split(' '); //Using the punctuation-free string, splits words at each whitespace
         }
 
-        private string MostFrequentWord (string[] text)
+        public string MostFrequentWord (string[] text)
         {
             return text.GroupBy(w => w).OrderByDescending(g => g.Count()).First().Key; //Creates a descending-by-size IGroup. Returns the first key
         }
 
-        private IGrouping<int, string> ThirdLongestWord(string[] text)
+        public IGrouping<int, string> ThirdLongestWord(string[] text)
         {
             var thirdWord = text.OrderByDescending(t => t.Count()).GroupBy(t => t.Length).Skip(2).First(); //Creates a descending-by-size/descending by count IGroup. Skips first two and takes the first, which is now the third largest.
             return thirdWord;
         }
-
-        public void FullAnalysis(string text) //putting it all together for the console
-        {
-            var thirdLargestWord = string.Join(", ", ThirdLongestWord(ToWordArray(text)).ToList());
-            var thirdLargestWordCharCount = ThirdLongestWord(ToWordArray(text)).Key;
-
-            Console.WriteLine("There are " + ToSentenceArray(text).Length + " sentences.");
-            Console.WriteLine("There are " + ToWordArray(text).Length + " total words.");
-            Console.WriteLine("The longest sentence is: " + LongestSentence(ToSentenceArray(text)));
-            Console.WriteLine("The most frequently used word is: \"" + MostFrequentWord(ToWordArray(text)) + "\"");
-            Console.WriteLine("The third longest word(s): " + thirdLargestWord + "   Number of characters: " + thirdLargestWordCharCount);
-            Console.ReadLine();
-        }
-
     }
 
 
